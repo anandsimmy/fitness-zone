41c3a43aa0f168fc59c766e72657dc62bcf1c617
Anand Simmy <anandsimmy@Anands-MacBook-Pro.local>
2022-12-31 09:09:02 +0530

    added db connection

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+// require database connection
+const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
+
+dbConnect();
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response, next) => {
-  response.json({ message: "Hey! This is your server response!" });
+app.get('/', (request, response, next) => {
+  response.json({ message: 'Hey! This is your server response!' });
   next();
 });
 
+app.post('/register', (request, response) => {
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // saving user to db
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: 'User Created Successfully',
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: 'Error creating user',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: 'Password was not hashed successfully',
+        e,
+      });
+    });
+});
 
 module.exports = app;
