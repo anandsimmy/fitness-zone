7bfb227ae7614f089cbb8dc72260759727e64856
Anand Simmy <anandsimmy@Anands-MacBook-Pro.local>
2023-01-04 08:54:24 +0530

    added login endpoint

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+// third party libs
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// first party libs
+const auth = require('./auth');
 
 // require database connection
 const dbConnect = require('./db/dbConnect');
 const User = require('./db/userModel');
 
 dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 // body parser configuration
 app.use(bodyParser.json());
@@ -16,6 +35,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response, next) => {
   response.json({ message: 'Hey! This is your server response!' });
   next();
+});
+
+// free endpoint
+app.get('/free-endpoint', (request, response) => {
+  response.json({ message: 'You are free to access me anytime' });
+});
+
+// authentication endpoint
+app.get('/auth-endpoint', auth, (request, response) => {
+  response.json({ message: 'You are authorized to access me' });
 });
 
 app.post('/register', (request, response) => {
@@ -46,6 +75,52 @@ app.post('/register', (request, response) => {
     .catch((e) => {
       response.status(500).send({
         message: 'Password was not hashed successfully',
+        e,
+      });
+    });
+});
+
+app.post('/login', (request, response) => {
+  User.findOne({ email: request.body.email })
+    .then((user) => {
+      bcrypt
+        .compare(request.body.password, user.password)
+        .then((passwordCheck) => {
+          // check if password matches
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: 'Passwords does not match',
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            'RANDOM-TOKEN',
+            { expiresIn: '24h' }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: 'Login Successful',
+            email: user.email,
+            token,
+          });
+        })
+        .catch((error) => {
+          response.status(400).send({
+            message: 'Passwords does not match',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(404).send({
+        message: 'Email not found',
         e,
       });
     });
---
+++ auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (request, response, next) => {
+  try {
+    const token = await request.headers.authorization.split(' ')[1];
+
+    //check if the token matches the supposed origin
+    const decodedToken = await jwt.verify(token, 'RANDOM-TOKEN');
+    console.log('decodedToken', decodedToken);
+
+    // retrieve the user details of the logged in user
+    const user = await decodedToken;
+    console.log('decodedToken user', user);
+
+    request.user = user;
+
+    // pass down functionality to the endpoint
+    next();
+  } catch (error) {
+    response.status(401).json({
+      error: new Error('Invalid request!'),
+    });
+  }
+};
